b5d6ef81f56ccc7a04c159ec4a0b02b3cb6c651a
minseong <kdsvip5@naver.com>
2022-07-14 16:40:57 +0900

    Rq, FileDB 테스트

--- test/AppTest.java
+++ test/AppTest.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Test;
-import util.Rq;
+import Request.Rq;
 import util.Util;
 
 import java.io.*;
@@ -12,41 +12,6 @@ public class AppTest {
     void 파일에_내용쓰기() {
         Util.mkdir("test_data");
         Util.saveToFile("test_data/1.json", "내용");
-    }
-
-    @Test
-    public void Rq__getPath() {
-        Rq rq = new Rq("삭제?id=1");
-
-        String path = rq.getPath();
-
-        assertEquals("삭제", path);
-    }
-
-    @Test
-    public void Rq__getIntParam() {
-        Rq rq = new Rq("삭제?id=1");
-
-        int id = rq.getIntParam("id", 0);
-
-        assertEquals(1, id);
-    }
-
-    @Test
-    public void Rq__getIntParam__2() {
-        Rq rq = new Rq("검색?id=10&no=1");
-
-        int id = rq.getIntParam("id", 0);
-        int no = rq.getIntParam("no", 0);
-
-        assertEquals(10, id);
-        assertEquals(1, no);
-    }
-
-    @Test
-    public void 테스트_실험() {
-        int rs = 10 + 20;
-        assertEquals(30, rs);
     }
 
     @Test
